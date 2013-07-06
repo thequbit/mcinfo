40bb9d778f20dad60e9e37f11c149e2de0c6d8ed
thequbit <thequbit@gmail.com>
2013-07-06 00:15:07 -0400

    added addresses table

--- db/create_db.sql
+++ db/create_db.sql
@@ -9,16 +9,32 @@ use mcinfodb;
 
 create table communities(
 communityid int not null auto_increment primary key,
-name varchar(127) not null,
+name varchar(128) not null,
 hrid int not null
 );
 
 create table streets(
 streetid int not null auto_increment primary key,
-rawname varchar(127) not null,
-name varchar(127) not null,
-streettype varchar(63) not null,
+rawname varchar(128) not null,
+name varchar(128) not null,
+streettype varchar(64) not null,
 communityid int not null,
 foreign key (communityid) references communities(communityid)
 );
 
+create table addresses(
+addressid int not null auto_increment primary key,
+rawaddress varchar(256) not null,
+fulladdress varchar(256) not null,
+number int not null,
+streetname varchar(256) not null,
+streettype varchar(64) not null,
+zipcode varchar(8) not null
+community varchar(256) not null,
+communitytype varchar(64),
+county varchar(256) not null,
+fullstate varchar(256) not null,
+stateabiv varchar(8) not null,
+lat double not null,
+lng double not null,
+);
